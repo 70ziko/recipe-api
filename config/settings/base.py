03c2a7ef171935ced3b739143c236e9419a8580d
PatrykF <103563386+70ziko@users.noreply.github.com>
2023-10-28 13:59:12 +0200

    static files fix tries

--- config/settings/base.py
+++ config/settings/base.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'frontend/build/static'
 PROJECT_ROOT = Path(BASE_DIR).resolve().parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'build')
 
