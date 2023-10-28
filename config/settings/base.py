7e68bacd266e2b4d985e0d1f23f34c584a129859
PatrykF <103563386+70ziko@users.noreply.github.com>
2023-10-28 13:24:46 +0200

    Fix frontend serving

--- config/settings/base.py
+++ config/settings/base.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
-PROJECT_ROOT = Path(BASE_DIR).resolve().parent.parent.parent
+PROJECT_ROOT = Path(BASE_DIR).resolve().parent
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'build/static')
 
 
