46457a298d61235137a7245b3290991fa8d9069b
PatrykF <103563386+70ziko@users.noreply.github.com>
2023-11-06 17:46:44 +0100

    dziala

--- config/settings/development.py
+++ config/settings/development.py
@@ -5,7 +5,9 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),  # Replace 'path_to_your_react_app' with the relative path to your React app's build folder
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
